bbb43e62b808f888214beac1ae1b2610be7dd65d
JanithSandaru <janithsandaru999@gmail.com>
2022-05-06 14:52:28 +0530

    index added

--- src/CustomerServlet.java
+++ src/CustomerServlet.java
@@ -4,11 +4,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doPut requested...");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("got");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Sended from 1... ");
     }
 }
